e74d48da30af0c70a2a29349ad7a7c166f8f09a9
UmmerZ <ummer.zaman88@gmail.com>
2020-10-21 11:11:10 -0600

    intial commit

---
+++ OOP/OOP/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace OOP
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        private int _age;
+
+        public string LastName
+        {
+            get
+            {
+
+                return _lastName;
+
+            }
+            set
+
+            {
+                _lastName = value;
+            }
+        }
+             public int userAge
+        {
+            get
+            {
+                
+                return _age; 
+               
+
+
+            }
+
+            set
+
+            {
+                if (userAge < 0)
+                { 
+                    throw new Exception("Not a valid age.please try again");
+                }
+                _age = value;
+                
+                
+            }
+        }
+        public void birthday()
+        {
+            userAge += 1;
+        }
+        public Person()
+        {
+            FirstName = "John";
+            LastName = "Doe";
+            userAge = 20;
+        }
+
+        // Partial Constructror - Takes parameters for some properties, and defaults others.
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            userAge = 20;
+        }
+
+        // Greedy Constructor - Takes paramaters for all properties.
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            userAge = age;
+        }
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            string firstName, lastName;
+            int userAge;
+            // Default Constructor
+            Person defaultPerson = new Person();
+            // Partial Constructor
+            Person partialPerson = new Person("Test", "Testson");
+            // Greedy Constructor
+            Person greedyPerson = new Person("Test", "McGreedy", 50);
+
+
+            Console.Write("Please enter your first name: ");
+            person.FirstName = Console.ReadLine().Trim();
+
+
+            Console.Write("Please enter your last name: ");
+            person.LastName = Console.ReadLine().Trim();
+
+            Console.Write("Please enter your age: ");
+            try
+            {
+                person.userAge = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message} Setting to zero.");
+                person.userAge = 0;
+            }
+            Console.WriteLine($"Hello, {person.FirstName} {person.LastName} who is {person.userAge} years old!");
+
+
+            person.birthday();
+            Console.WriteLine($"You just had a birthday, you are now {person.userAge} years old.");
+            
+        }
+        
+    }
+}
